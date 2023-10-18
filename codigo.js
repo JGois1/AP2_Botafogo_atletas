a1b09421511d4f5aa7eae018130dfbe962ea554b
João Gois de Otoni <127162053+JGois1@users.noreply.github.com>
2023-10-18 09:14:54 -0300

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -8,10 +8,21 @@ body.appendChild(div_container);
 const cria_cartao = (atleta) => {
 
     const container_atleta = document.createElement('div');
+    //aplicando css aqui nas divs
+    container_atleta.style.width = '20rem';
+    container_atleta.style.backgroundColor = '#777777';
+    container_atleta.style.textAlign = 'center';
+    container_atleta.style.margin = 'auto';
+    container_atleta.style.fontFamily = 'system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif';
+    container_atleta.style.borderRadius = '5px';
+    
     const titulo = document.createElement('h3');
     titulo.innerHTML = atleta.nome;
+    
     const imagem = document.createElement('img');
+    imagem.style.borderRadius = '5px';
     imagem.src = atleta.imagem;
+    imagem.alt = `foto de ${atleta.nome}`; // criando imagem alternativa na img
     const descricao = document.createElement('p');
     descricao.innerHTML = atleta.descricao;
 
@@ -22,11 +33,13 @@ const cria_cartao = (atleta) => {
     div_container.appendChild(container_atleta);
 }
 
-/* 4 TIPOS DIFERENTES DE LOOPS (OS 4 DÃO CERTO IGUAL) : 
-// 1º MAIS SIMPLES
+/* 
+3 TIPOS DIFERENTES DE LOOPS (OS 3 DÃO CERTO IGUAL) ->
+1º MAIS SIMPLES
  atletas.forEach((atleta) => {cria_cartao(atleta)});
-
-// 2º WHILE, VERSÁTIL
+*/
+/*
+2º WHILE, VERSÁTIL
 let indice = 0; //criando contador
 
 while (indice < atletas.length){
@@ -34,14 +47,16 @@ while (indice < atletas.length){
     console.log(indice);
     indice++;
 }
-
+*/
+/*
 // 3º MAIS UNIVERSAL
 for(let indice =0;indice < atletas.length;indice++){  // em vez de indice podemos escrever i, como no python/c++
     cria_cartao(atletas[indice]);
     console.log('loop for', indice);
 }
 */
-// 4º "for of" IGUAL O DO PYTHON
+
+//4º IGUAL O DO PYTHON
 for(const atleta of atletas){
     cria_cartao(atleta);
     console.log('loop for of', atleta.id);
